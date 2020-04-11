518bf17309c5960ba3d1c3c4c9fa91e0d598eca9
synthesissoftware <61877672+synthesissoftware@users.noreply.github.com>
2020-04-11 11:18:18 +0800

    Google Maps

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,24 +1,26 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "a2650a77d9a5a5dcd1cbd0a2c07aaa83",
+  "assets/AssetManifest.json": "43872bd6f816fcbb43d032f70999f5e4",
 "assets/assets/logo.gif": "6562075b0972542d53e3a7f5c5d7aa76",
 "assets/assets/logo.png": "01f2d88adb9d98687b00391691d5c156",
 "assets/assets/patient.png": "cc510715130e6ce6771462bf83b570c4",
 "assets/assets/splashscreen.gif": "baa2643255262d85bf2c171bd223877d",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "30990b8e171f7664a21b4220ac42df90",
+"assets/LICENSE": "53f6c4ddb1d25079ccfd001ed5e44b0d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/flutter_google_maps/assets/images/marker_a.png": "9b687e681fcc41298dfab9c1304b66d0",
+"assets/packages/flutter_google_maps/assets/images/marker_b.png": "bb8df73622307b582f89602743f4f02f",
 "assets/packages/flutter_google_places/assets/google_black.png": "97f2acfb6e993a0c4134d9d04dff21e2",
 "assets/packages/flutter_google_places/assets/google_white.png": "40bc3ae5444eae0b9228d83bfd865158",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "e9b1a363c9aa52c2322bb82da498d9e6",
-"/": "e9b1a363c9aa52c2322bb82da498d9e6",
-"main.dart.js": "f85d9969d90ba785aa1599c5c0188013",
-"manifest.json": "21e667839fceb1c989c7f30db175b6cd"
+"index.html": "6b84847f1c05fbacda7a4fe4d95f1472",
+"/": "6b84847f1c05fbacda7a4fe4d95f1472",
+"main.dart.js": "5bfec06f32e8ddc6219bd9cc9ebb945e",
+"manifest.json": "dfc978749d9cd6f31ae84744242c5562"
 };
 
 self.addEventListener('activate', function (event) {
